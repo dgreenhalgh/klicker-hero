ef0b9f8fb189f8e0784f48366309015e927694dd
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-19 15:21:38 -0400

    Converted main activity to Kotlin

---
+++ KlickerHero/app/src/main/java/com/dgreenhalgh/android/klickerhero/KlickerActivity.kt
@@ -0,0 +1,12 @@
+package com.dgreenhalgh.android.klickerhero
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class KlickerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_klicker)
+    }
+}
